9ffff9ba3c51db432b22d7b30285ba803f0d3034
Aakash <98047763+Aak172003@users.noreply.github.com>
2023-01-25 15:21:41 +0530

    Add files via upload

---
+++ Codeing-Part/Binary - Search(  Babbar )/Peak-Index-In a Mountain-Array.cpp
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/peak-index-in-a-mountain-array/solutions/3057752/brute-force-efficient-approach-how-to-think-of-efficient-approach-explained-why-binary-search/
+
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) 
+    {
+        int n=arr.size();
+        int left=0,right=n-1;
+
+        int mid=(right-left)/2+left;
+
+        while(left<right)
+        {
+            //  For Peak Condition
+            if(arr[mid-1]<arr[mid] && arr[mid+1]<arr[mid])
+            return mid;
+
+            else if(arr[mid]<arr[mid+1])
+            left=mid+1;
+
+            else 
+            right=mid;
+
+            mid=(right-left)/2+left;
+        }
+        return mid;  
+    }
+};
