9e3dcb8d21709dd1308bc189ede9b9930a02c4f3
Aakash <98047763+Aak172003@users.noreply.github.com>
2023-01-25 17:23:19 +0530

    Update Find-Pivot-Index( Leetcode) .cpp

--- Codeing-Part/Babbar -( Array  )/Find-Pivot-Index( Leetcode) .cpp
+++ Codeing-Part/Babbar -( Array  )/Find-Pivot-Index( Leetcode) .cpp
@@ -8,56 +8,7 @@ public:
     int pivotIndex(vector<int>& nums) 
     {
         // Brute Force Approach 
-        /*
-        TC- O(N^2)
-        SC - o(1)
-        */
-
-        // for(int i=0;i<nums.size();i++)
-        // {
-        //     int leftsum=0,rightsum=0;
-
-        //     for(int j=0;j<i;j++)
-        //     {
-        //         leftsum = leftsum+nums[j];
-        //     }
-        //     for(int k=nums.size()-1;k>i;k--)
-        //     {
-        //         rightsum = rightsum+ nums[k];
-        //     }
-        //     if(leftsum==rightsum)return i;
-
-        // }
-        // return -1;
-
-        // Prefix sum 
-        /*
-        TC- O(N)
-        SC - o(1)
-        */
-
-        int sum=0;
-        for(int i=0;i<nums.size();i++)
-        {
-            sum = sum+nums[i];
-        }
         
-        int lsum = 0;
-        int rsum = sum;
-
-        for(int i=0;i<nums.size();i++)
-        {
-            rsum = rsum-nums[i];
-            if(lsum==rsum)return i;
-            lsum = lsum+nums[i];
-        }
-        return -1;
-    }
-};class Solution {
-public:
-    int pivotIndex(vector<int>& nums) 
-    {
-        // Brute Force Approach 
         /*
         TC- O(N^2)
         SC - o(1)
