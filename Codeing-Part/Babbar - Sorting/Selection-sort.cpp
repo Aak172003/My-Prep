7c7c381cad96520b42cdd9bf09b5485359ed92e9
Aakash <98047763+Aak172003@users.noreply.github.com>
2023-01-27 00:05:15 +0530

    Update Selection-sort.cpp

--- Codeing-Part/Babbar - Sorting/Selection-sort.cpp
+++ Codeing-Part/Babbar - Sorting/Selection-sort.cpp
@@ -4,14 +4,17 @@ void selectionSort(vector<int>& arr, int n)
     // Write your code here.
     for(int i=0;i<n;i++)
     {
+        // Take 1st As minimum Value index
         int min_idx =  i;
         for(int j=i+1;j<n;j++)
         {
+            // Compare if any value is lesser than our assumption value so update minimum index
             if(arr[min_idx]>arr[j])
             {
                 min_idx = j;
             }
         }
+        // Swap uing Scratch Method
          int temp = arr[min_idx];
          arr[min_idx]=arr[i];
          arr[i]=temp;
