d1158cc771a9d9bc2860039fd884086f9c69a2f6
Aakash <98047763+Aak172003@users.noreply.github.com>
2023-04-16 00:20:12 +0530

    Add files via upload

---
+++ 13 - Reccursion & Backtracking/Coding - Part ( Reccursion )/3 - Fibonacci - No .cpp
@@ -0,0 +1,52 @@
+/*
+LeetCode Link - https://leetcode.com/problems/fibonacci-number/description/
+*/
+
+class Solution {
+public:
+    int fib(int n) {
+        /*
+        Both Way 
+        TC - O(N)
+        SC - Auxillary Space 
+        */
+        
+        //  Using Reccursion Solution 
+        if(n<=1)return n;
+
+        // Or 
+
+        // if(n==0)return 0;
+        // if(n==1)return 1;
+
+
+        int last = fib(n-1);
+        int secondLast = fib(n-2);
+
+        int result  = last+secondLast;
+        return result ;
+
+        
+        //  Using Iterative way    
+
+        if(n<=1)return n;
+
+        // Or 
+
+        // if(n==0)return 0;
+        // if(n==1)return 1;
+        
+        int prev1=0;
+        int prev2=1,nextelement;
+        for(int i=2;i<=n;i++)
+        {
+            nextelement = prev1 + prev2;
+            prev1=prev2;
+            prev2=nextelement;
+        }
+        return nextelement;
+
+    }
+};
+
+
